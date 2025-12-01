07fc0ca98344eb9f13ea7eb71166508939aab876
Karthikeya-g <na21b026@smail.iitm.ac.in>
2025-12-01 16:12:53 +0530

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from .forward_kinematics import ForwardKinematics, JointState
+
+# This allows users to import them directly from the package
+# Example: from src import ForwardKinematics
+__all__ = ["ForwardKinematics", "JointState"]
